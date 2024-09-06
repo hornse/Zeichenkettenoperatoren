c0d0939cd6146aad46dd0d6cf8835e59b36e405e
Seb <hornse@arcor.de>
2024-09-06 11:44:32 +0200

    Mit Menü

--- src/Tester.java
+++ src/Tester.java
@@ -4,42 +4,50 @@
  * @author Sebastian Horn
  * @version 0.1
  */
-public class Tester
-{
+public class Tester {
     // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private String zEingabe;
-    private String zAusgabe;
+    private String eingabe;
+    private String ausgabe;
+    private boolean istPalindrom;
 
     /**
      * Konstruktor für Objekte der Klasse Tester. Ein Objekt wird erzeugt und die beiden Attribute zEingabe und zAusgabe
      * initialisiert.
      */
     public Tester() {
-        zEingabe = "";
-        zAusgabe = "";
+        eingabe = "";
+        ausgabe = "";
+        istPalindrom = false;
     }
 
-    public String getZEingabe() {
+    public String getEingabe() {
+        return eingabe;
+    }
+
+    public String getAusgabe() {
+        return ausgabe;
+    }
+
+    public void setEingabe(String pEingabe) {
         //dein Quellcode hier
     }
 
-    public String getZAusgabe() {
-        //dein Quellcode hier
-    }
-
-    public void setZEingabe(String pEingabe) {
-        //dein Quellcode hier
-    }
-
-    public void setZAusgabe(String pAusgabe) {
+    public void setAusgabe(String pAusgabe) {
         //dein Quellcode hier
     }
 
     public boolean palindromTest() {
         //dein Quellcode hier
+        return istPalindrom;
     }
 
     public String zeichenketteUmkehren() {
+        String umgedrehtes ="";
         //dein Quellcode hier
+        return  umgedrehtes;
+    }
+
+    public boolean isIstPalindrom() {
+        return istPalindrom;
     }
 }
--- src/Zeichenkettenoperationen.java
+++ src/Zeichenkettenoperationen.java
@@ -13,37 +13,30 @@ public class Zeichenkettenoperationen {
     public Zeichenkettenoperationen() {
         test = new Tester();
         scanner = new Scanner(System.in);
-
         while (true) {
             System.out.println("== HAUPTMENÜ ==");
             System.out.println("[1] Zeichenkette umdrehen");
             System.out.println("[2] Palindromtest");
-            System.out.println("[3] Team mit längsten Namen ausgeben");// (" + this.countNewMessages + ")");
-            System.out.println("[4] Team mit höchsten Punktestand angeben");
-            //System.out.println("[99] Fake Nachricht empfangen");
-            //System.out.println("[0] Beenden");
+            System.out.println("[0] Beenden");
             int option = scanner.nextInt();
             scanner.nextLine(); // nextInt scannt keine neue Zeile. Ohne diese Anweisung würde das nächste gewollte nextLine nicht funktionieren.
             if (option == 1) {
-
-                this.test.setZEingabe();
+                System.out.println("Gib die Zeichenkette an, die umgekehrt werden soll:");
+                String zeichenkette = scanner.nextLine();
+                this.test.setEingabe(zeichenkette);
                 this.test.zeichenketteUmkehren();
 
             } else if (option == 2) {
                 // this.sendMessage();
-                System.out.println("Bitte den Namen des neuen Teams angeben.");
-                String neuesTeam = scanner.nextLine();
-                this.vw.fuegeTeamDerTabelleZu(new Team(neuesTeam));
-                this.vw.gibAlleTeamsAus();
-            } else if (option == 3) {
-                //this.showNotifications();
-            } else if (option == 4) {
-                //  this.deleteNotifications();
-            } else if (option == 0) {
+                System.out.println("Gib die Zeichenkette für den Palindromtest an:");
+                String zeichenkette = scanner.nextLine();
+                this.test.setEingabe(zeichenkette);
+                this.test.palindromTest();
+                System.out.println("Ist dein Wort ein Palindrom? Die Antwort lautet: "+this.test.isIstPalindrom());
+                }
+            else if (option == 0) {
                 break;
-            } else if (option == 99) {
-                // this.receiveFakeMessage();
-            }
+        }
             System.out.println();
         }
     }
