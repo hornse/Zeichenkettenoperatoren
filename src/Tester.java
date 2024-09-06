494109579a63464ac70fdfb5b86b2e318b219c87
Seb <hornse@arcor.de>
2024-09-06 11:31:34 +0200

    initialexport

--- src/Tester.java
+++ src/Tester.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class Tester {
+/**
+ * Beschreiben Sie hier die Klasse Tester.
+ *
+ * @author Sebastian Horn
+ * @version 0.1
+ */
+public class Tester
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private String zEingabe;
+    private String zAusgabe;
+
+    /**
+     * Konstruktor für Objekte der Klasse Tester. Ein Objekt wird erzeugt und die beiden Attribute zEingabe und zAusgabe
+     * initialisiert.
+     */
+    public Tester() {
+        zEingabe = "";
+        zAusgabe = "";
+    }
+
+    public String getZEingabe() {
+        //dein Quellcode hier
+    }
+
+    public String getZAusgabe() {
+        //dein Quellcode hier
+    }
+
+    public void setZEingabe(String pEingabe) {
+        //dein Quellcode hier
+    }
+
+    public void setZAusgabe(String pAusgabe) {
+        //dein Quellcode hier
+    }
+
+    public boolean palindromTest() {
+        //dein Quellcode hier
+    }
+
+    public String zeichenketteUmkehren() {
+        //dein Quellcode hier
+    }
 }
--- src/Zeichenkettenoperationen.java
+++ src/Zeichenkettenoperationen.java
@@ -1,5 +1,53 @@
-public class Main {
+import java.util.Scanner;
+
+public class Zeichenkettenoperationen {
+    private Scanner scanner;
+    private Tester test;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new Zeichenkettenoperationen();
+        System.out.println("Herzlich willkommen zuZeichenketten Tester!");
+
     }
+
+    public Zeichenkettenoperationen() {
+        test = new Tester();
+        scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("== HAUPTMENÜ ==");
+            System.out.println("[1] Zeichenkette umdrehen");
+            System.out.println("[2] Palindromtest");
+            System.out.println("[3] Team mit längsten Namen ausgeben");// (" + this.countNewMessages + ")");
+            System.out.println("[4] Team mit höchsten Punktestand angeben");
+            //System.out.println("[99] Fake Nachricht empfangen");
+            //System.out.println("[0] Beenden");
+            int option = scanner.nextInt();
+            scanner.nextLine(); // nextInt scannt keine neue Zeile. Ohne diese Anweisung würde das nächste gewollte nextLine nicht funktionieren.
+            if (option == 1) {
+
+                this.test.setZEingabe();
+                this.test.zeichenketteUmkehren();
+
+            } else if (option == 2) {
+                // this.sendMessage();
+                System.out.println("Bitte den Namen des neuen Teams angeben.");
+                String neuesTeam = scanner.nextLine();
+                this.vw.fuegeTeamDerTabelleZu(new Team(neuesTeam));
+                this.vw.gibAlleTeamsAus();
+            } else if (option == 3) {
+                //this.showNotifications();
+            } else if (option == 4) {
+                //  this.deleteNotifications();
+            } else if (option == 0) {
+                break;
+            } else if (option == 99) {
+                // this.receiveFakeMessage();
+            }
+            System.out.println();
+        }
+    }
+
+
+
 }
